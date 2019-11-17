a469bbd416f2961ae18032138a116d7a2d1a9a7e
Eric Lee <Twotex187@hotmail.com>
2019-11-16 17:11:50 -0800

    Starting out this Project. Not much done yet. Just creating repo.

---
+++ ForkProject.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int findCount(string theString, int theLength);
+int main()
+{
+    string theInput = "Fam Bam Sham Hellacram Juicy";
+    int theLength = 3;
+
+    cout << "55" << endl;
+
+
+
+    
+    return 0;
+}
+
+
+int findCount(string theString, int theLength)
+{
+    return 1;
+}
